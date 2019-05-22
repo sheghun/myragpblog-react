aea2c61dd8c88ac575329e530eea069504f72bfd
Oladiran Segun Solomon <sheghunoladiran9@gmail.com>
2019-05-22 21:59:32 +0100

    Changed the spinner animation

---
+++ src/Components/Spinner/Spinner.tsx
@@ -0,0 +1,14 @@
+import React from "react"
+import "./Spinner.css";
+
+const Spinner = () => {
+	return (
+		<div className="loader-block">
+			<div className="loader-block-container">
+				<div className="circle-block circle-block-style-6" />
+			</div>
+		</div>
+	);
+};
+
+export default Spinner;
