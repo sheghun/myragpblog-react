57f799ee6c64a9e33066288bdc952f205e0ac093
Oladiran Segun Solomon <sheghunoladiran9@gmail.com>
2019-08-05 19:22:28 +0100

    Redesigned the whole page

---
+++ src/index.tsx
@@ -0,0 +1,32 @@
+import "./bootstrap";
+// tslint:disable-next-line: ordered-imports
+import axios from "axios";
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+import "./index.css";
+import * as serviceWorker from "./serviceWorker";
+import store from "./Store/configureStore";
+
+axios.defaults.baseURL = "http://localhost:8000";
+// axios.defaults.baseURL = 'https://api.myragpblog.com';
+axios.defaults.headers.common["Content-Type"] = "application/json";
+axios.defaults.withCredentials = true;
+const RCRedux = () => {
+	return (
+		<BrowserRouter>
+			<Provider store={store}>
+				{
+					// @ts-ignore
+					<App />
+				}
+			</Provider>
+		</BrowserRouter>
+	);
+};
+
+ReactDOM.render(<RCRedux />, document.getElementById("root"));
+
+serviceWorker.unregister();
